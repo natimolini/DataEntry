4f0f14089e71ea7933c99aaa4c912e27be505419
Natália Molini Buffa <nati.buffa1907@gmail.com>
2024-11-28 11:49:07 -0300

    organização

--- footer.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="css/footer.css">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Jockey+One&display=swap" rel="stylesheet">
-<footer class="container">
-    &copy; 2024 All Rights Reserved GHR Business Solutions
-</footer>
